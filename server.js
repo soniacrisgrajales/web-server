6c0ad69904a0bfed3d568018ab263e31be2a5202
Sonia Grajales <sonia.cris.grajales.ramirez@gmail.com>
2017-01-23 09:26:52 -0500

    Refactor middleware

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
+var middleware = require('./middleware.js');
 
 // app.get('/', function(req, res){
 // 	res.send('Hello Express!')
 // });
 
-var middleware = {
-	requireAuthentication: function(req, res, next) {
-		console.log('private route hit');
-		next();
-	},
-	logger: function(req, res, next){
-		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-}
 
 app.use(middleware.logger);
 
